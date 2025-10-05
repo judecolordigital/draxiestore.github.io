be8f91cb3d0327a5fba359fae7ce715cb63f7344
Kato Drake Smith <judecolordigital@gmail.com>
2025-10-05 22:22:38 +0300

    update

--- 6ammart-react/src/utils/gtm.js
+++
@@ -1,7 +0,0 @@
-import TagManager from 'react-gtm-module';
-
-const GTM_ID = 'GTM-MF2PDDZ';
-
-export const initGTM = () => {
-  TagManager.initialize({ gtmId: GTM_ID });
-};
--- 6ammart-react/src/utils/setDefaultLanguage.js
+++
@@ -1,6 +0,0 @@
-export const setDefaultLanguage = () => {
-  const lan = "ar";
-  const country = "SA";
-  localStorage.setItem("language-setting", JSON.stringify(lan));
-  localStorage.setItem("country", JSON.stringify(country));
-};
